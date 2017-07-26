560de51b7da872e4817e76934e2552c088e411b0
Karl Krueger <karl@udacity.com>
2017-07-26 10:21:07 -0700

    Update README.md

--- Lesson-2/0_HelloServer/HelloServer.py
+++ Lesson-2/0_HelloServer/HelloServer.py
@@ -17,9 +17,9 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write("Hello, HTTP!\n".encode()) #connection from the server to the client (writes binary data)
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
     httpd = HTTPServer(server_address, HelloHandler)
-    httpd.serve_forever()
+    httpd.serve_forever() #starts the web server running
